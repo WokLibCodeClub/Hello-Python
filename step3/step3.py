152ad92bcb46ebd5ebdde944551322b224ffa8ae
simonc8 <scrombie@waitrose.com>
2024-12-11 13:20:26 +0000

    Create step3.py

---
+++ step3/step3.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+from turtle import *
+
+s = Screen()
+s.setup(400,400)
+s.bgcolor('lightblue')
+
+t = Turtle()
+t.shape('turtle')
+t.pencolor('red')
+t.fillcolor('yellow')
+
+t.penup()
+t.goto(-50,-50)
+t.pendown()
+
+num_sides = 12
+size = 400 / num_sides
+
+t.begin_fill()
+counter = 0
+while counter < num_sides:
+  t.forward(size)
+  t.left(360 / num_sides)
+  counter += 1
+t.end_fill()
